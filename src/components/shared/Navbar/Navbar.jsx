53e79da3a11e8589f1128bc648222bad15019b1d
Kausar Ahmad Tasin <67546210+KausarAhmadTasin@users.noreply.github.com>
2024-12-03 04:51:55 +0600

    Update Navbar.jsx

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -18,14 +18,14 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <div className="w-full fixed top-0 bg-[#111827] z-40 py-6 select-none custom-navbar-shadow">
+    <div className="w-full fixed top-0 bg-[#111827] z-40 py-4 md:py-6 select-none custom-navbar-shadow">
       <div className="container mx-auto flex justify-between items-center">
         <Link href={"/"}>
           <h1 className="text-3xl font-bold bg-gradient-to-r cursor-pointer from-indigo-400 via-purple-500 to-orange-500 text-transparent bg-clip-text active:scale-95">
             User Management
           </h1>
         </Link>
-        <ul className="flex justify-between items-center gap-8">
+        <ul className="hidden md:flex justify-between items-center gap-8">
           {navLinks.map((link) => (
             <Link href={link.path} key={link.path}>
               <li className="text-slate-200 text-lg font-medium hover:underline underline-offset-4">
