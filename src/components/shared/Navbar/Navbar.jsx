10833a1b3bf3f35d18bf47dcdcec56cae2999e4c
KausarAhmadTasin <kausarahmadtasinkat@gmail.com>
2024-12-02 18:51:14 +0600

    first commit

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+const navLinks = [
+  {
+    title: "Create Use",
+    path: "createUser",
+  },
+  {
+    title: " All Users",
+    path: "allUsers",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-[#111827] py-4 select-none">
+      <div className="container mx-auto flex justify-between">
+        <Link href={"/"}>
+          <h1 className="text-3xl font-bold bg-gradient-to-r cursor-pointer from-indigo-400 via-purple-500 to-orange-500 text-transparent bg-clip-text active:scale-95">
+            User Management
+          </h1>
+        </Link>
+        <ul className="flex justify-between items-center gap-8">
+          {navLinks.map((link) => (
+            <Link href={link.path} key={link.path}>
+              <li className="text-slate-200 text-lg font-medium hover:underline underline-offset-4">
+                {link.title}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
